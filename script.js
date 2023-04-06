47b6ed6e4a2519e30d68f7b0ae83ad6081a7837e
Mahi <pasarkarmahi@gmail.com>
2023-04-05 22:48:24 -0400

    stuff

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ $('.btn').click(function(){
     $('.sidebar').toggleClass("show");
 });
 
-$('.portfolio-btn').click(function(){
+$('.portfolio-btn').click(function(e){
+    e.preventDefault();
     $('nav ul .portfolio-show').toggleClass("show");
     $('nav ul .first').toggleClass("rotate");
 });
 
 //Experience Box
+$('.experience-box').click(function(){
+    console.log("hi");
+    $('.experience-info').toggle(1000);
+});
+
+function ExperienceBox(id){
+    var info = document.getElementById(id);
+    //info.toggle(1000);
+    info.hide();
+}
