90a3c7d77cc0fd341ee9a491a1b8e948d1666865
Mahi <mahipasarkar@gmail.com>
2023-05-08 19:07:09 -0400

    Fix Navbar

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-
-
-// Sidebar 
-$('.btn').click(function(){
+// Sidebar
+$(".btn").click(function () {
     $(this).toggleClass("click");
-    $('.sidebar').toggleClass("show");
+    $(".sidebar").toggleClass("show");
 });
 
-$('.portfolio-btn').click(function(e){
+$(".portfolio-btn").click(function (e) {
     e.preventDefault();
-    $('nav ul .portfolio-show').toggleClass("show");
-    $('nav ul .first').toggleClass("rotate");
+    $("nav ul .portfolio-show").toggleClass("show");
+    $("nav ul .first").toggleClass("rotate");
 });
+
 
 //Experience Box
-$('.experience-box').click(function(){
+$(".experience-box").click(function () {
     console.log("hi");
-    $('.experience-info').toggle(1000);
+    $(".experience-info").toggle(1000);
 });
 
-function ExperienceBox(id){
+function ExperienceBox(id) {
     var info = document.getElementById(id);
     //info.toggle(1000);
     info.hide();
 }
+
