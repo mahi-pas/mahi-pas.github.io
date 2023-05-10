1a336fdbc7c19f9a51428c964d098f8db24578d2
Mahi <mahipasarkar@gmail.com>
2023-05-10 13:55:41 -0400

    formatting

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 let mesh_mouse_hover = document.getElementById("mesh-mouse-hover");
 const onMouseMove = (e) => {
     const style = getComputedStyle(mesh_mouse_hover);
-    mesh_mouse_hover.style.left = `calc(${e.pageX}px - (${style.width}/2))`;
-    mesh_mouse_hover.style.top = `calc(${e.pageY}px - (${style.height}/2))`;
+    mesh_mouse_hover.style.left = `calc(${e.clientX}px - (${style.width}/2))`;
+    mesh_mouse_hover.style.top = `calc(${e.clientY}px - (${style.height}/2))`;
 };
 document.addEventListener("mousemove", onMouseMove);
 
