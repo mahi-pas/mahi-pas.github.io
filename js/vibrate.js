47586ccb469641d407659435847a764546522930
Mahi <mahipasarkar@gmail.com>
2023-07-21 16:06:26 -0400

    vibrate testing

---
+++ js/vibrate.js
@@ -0,0 +1,7 @@
+
+
+function vibrate() {
+    if (navigator) {
+        navigator.vibrate([2000,1000,300]);
+    }
+}
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+import "js/vibrate.js";
 
 // Animated Mesh Mouse Hover
 let mesh_mouse_hover = document.getElementById("mesh-mouse-hover");
